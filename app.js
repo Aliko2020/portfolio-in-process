bbc9d6877f95d532f4b51a6d6e54c92626396d5b
FixIT Computers <alikoamos7@gmail.com>
2023-04-28 16:39:52 +0100

    read more

--- app.js
+++ app.js
@@ -3,3 +3,16 @@ const navbar = document.querySelector('.nav-bar');
 hamburger.addEventListener('click', ()=>{
     navbar.classList.toggle("active");
 });
+
+// readmore sellection
+
+const readmore = document.querySelector('.read')
+const moreAboutMe = document.querySelector('.more-about-me')
+
+readmore.addEventListener('click',()=>{
+    if(moreAboutMe.style.display='none'){
+        moreAboutMe.style.display='block';
+    }else{
+        moreAboutMe.style.display= 'none';
+    }
+});
